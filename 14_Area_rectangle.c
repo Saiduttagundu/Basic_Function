86062c0274a3b8f6a26210c5ecb2c37b6a568d46
Saidutta Gundu <saidattagundu15@gmail.com>
2025-11-03 19:12:09 +0530

    Add files via upload

---
+++ 14_Area_rectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float area(float l, float b) 
+{
+    return l * b;
+}
+
+int main() 
+{
+    float l, b;
+    printf("Enter length and breadth: ");
+    scanf("%f%f", &l, &b);
+    
+    printf("Area = %.2f", area(l, b));
+    return 0;
+}
