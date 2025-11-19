c1f1178c33c0a1043794599360c6a4074bddf156
Saidutta Gundu <saidattagundu15@gmail.com>
2025-11-19 23:14:33 +0530

    Add multiplication table function and main

---
+++ 18_Table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void printTable(int n) {
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printTable(num);
+    return 0;
+}
